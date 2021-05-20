dd8a36e1a007c14e9a92bc1b2c689f003f80dca6
Raul.Laredo <Raul.Laredo@niceincontact.com>
2021-05-19 21:51:41 -0400

    Add project files.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,39 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public List<CityDto> Cities { get; set; }
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id=1,
+                    Name="New Your City",
+                    Description = "The one with that big park"
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Cochabamba",
+                    Description = "The one with that big park"
+                },
+                 new CityDto()
+                {
+                    Id = 3,
+                    Name = "La Paz",
+                    Description = "The great mountains and beautiful people."
+                }
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        //public JsonResult GetCities()
+        public IActionResult GetCities()
+        {
+            //return new JsonResult(
+            //    new List<object>(){
+            //    new { id=1, Name="New Your City"},
+            //    new { id = 2, Name = "Cochabamba City" }
+            //});
+
+            //return new JsonResult(CitiesDataStore.Current.Cities);
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if(cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityToReturn);
+        }
+    }
+}
